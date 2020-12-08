094d96966b91505611ef0a3558295b2cc435f54c
aydentownsley <aydentownsley@gmail.com>
2020-12-08 21:14:30 +0000

    pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         num = 0
         if ord(i) >= 97 and ord(i) <= 122:
-        	num = 32
+            num = 32
         print('{:c}'.format(ord(i) - num), end='')
     print()
