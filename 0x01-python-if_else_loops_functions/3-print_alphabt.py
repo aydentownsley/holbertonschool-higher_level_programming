8fbb44a175ccc4045cc0d71e17e0fa7c1f9fd47b
aydentownsley <aydentownsley@gmail.com>
2020-12-08 19:44:59 +0000

    changing 2 and 3 to use :c

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i != 16 and i != 4:
-        print(chr(97 + i), end='')
+for i in range(97, 122):
+    if i != 101 and i != 113:
+        print('{:c}'.format(i), end='')
