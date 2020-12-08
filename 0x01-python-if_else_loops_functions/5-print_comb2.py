fdf925081b63bee2938c9ebdda4514f478f4b2e6
aydentownsley <aydentownsley@gmail.com>
2020-12-08 19:59:37 +0000

    pep8 for 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(100):
     if i < 10:
         print('0{:d}, '.format(i), end='')
     elif i == 99:
-		print('{:d}'.format(i))
+        print('{:d}'.format(i))
     else:
         print('{:d}, '.format(i), end='')
