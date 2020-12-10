87419889b83c8140a858fd118bd54e1c63ac5f2c
aydentownsley <aydentownsley@gmail.com>
2020-12-10 18:23:26 +0000

    fixed arg count.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     while i < len(sys.argv):
         print("{:d}: {}".format(i, sys.argv[i]))
         i += 1
