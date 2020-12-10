64fe4af9aba20a54a4ae3a0210c3048241abca9a
aydentownsley <aydentownsley@gmail.com>
2020-12-10 18:21:38 +0000

    adding problem 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 1
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(sys.argv)))
+    while i < len(sys.argv):
+        print("{:d}: {}".format(i, sys.argv[i]))
+        i += 1
