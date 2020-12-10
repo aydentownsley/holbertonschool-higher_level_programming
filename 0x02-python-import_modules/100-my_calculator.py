df0f1d82dd29d8cc510b29f3aa5b0c785d8d2d46
aydentownsley <aydentownsley@gmail.com>
2020-12-10 20:30:15 +0000

    adding problem 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        exit(1)
+    op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if op == '+':
+        print('{} {} {} = {}'.format(a, op, b, add(a,b)))
+    elif op == '-':
+        print('{} {} {} = {}'.format(a, op, b, sub(a,b)))
+    elif op == '*':
+        print('{} {} {} = {}'.format(a, op, b, mul(a,b)))
+    elif op == '/':
+        print('{} {} {} = {}'.format(a, op, b, div(a,b)))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
