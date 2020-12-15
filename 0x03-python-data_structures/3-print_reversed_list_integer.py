1d18612a1d7d4b72ca9751d6abe0590ba121c8c6
aydentownsley <aydentownsley@gmail.com>
2020-12-15 22:52:09 +0000

    adding correct my list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,7 +8,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
+    if not my_list
+        return None
     for i in range(len(my_list) - 1, -1, -1):
         print('{:d}'.format(my_list[i]))
