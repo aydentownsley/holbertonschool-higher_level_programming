26983143facb9444cb30161bbe7b3d6bfb4862f8
aydentownsley <aydentownsley@gmail.com>
2020-12-15 22:45:58 +0000

    changed empty list to print()

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,6 +9,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        print()
     for i in range(len(my_list) - 1, -1, -1):
         print('{:d}'.format(my_list[i]))
