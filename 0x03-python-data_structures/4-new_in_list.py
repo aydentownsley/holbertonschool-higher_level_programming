1977f157ae386e425761263fa32521a0ca7dc547
aydentownsley <aydentownsley@gmail.com>
2020-12-15 22:58:59 +0000

    adding problem 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# new_in_list - replaces nth element in a list
+#
+# @my_list: list to be searched
+# @idx: location of object to be replaces
+#
+# Return: new copy of list with replacement
+# OR original list if idx is (-) OR out of range
+
+
+def new_in_list(my_list, idx, element):
+    if idx > len(my_list):
+        return my_list
+    a = my_list[:]
+    a[idx] = element
+    return a
