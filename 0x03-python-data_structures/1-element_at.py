e39234efb2be548fc478e906ae2eb589e1334906
aydentownsley <aydentownsley@gmail.com>
2020-12-15 21:41:58 +0000

    adding 0 and 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#
+# element_at - retrieves element at index from list
+#
+# @my_list: list to search
+# @idx: location to search at
+#
+# Result: return element at location if exists
+# OR None if it does not
+
+def element_at(my_list, idx):
+    i = 1
+    if idx > len(my_list):
+        return None
+    for i in range(len(my_list)):
+        if i == idx:
+            return (my_list[i])
