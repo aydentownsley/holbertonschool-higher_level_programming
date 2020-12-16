cb7059b753021d190af6e776b65642a462baf775
aydentownsley <aydentownsley@gmail.com>
2020-12-16 02:08:16 +0000

    adding 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# divisible_by_2 - finds all mutliples
+# of 2 in a list
+#
+# @my_list: list to search
+#
+# Return: List of True/False values
+# depending on divisible by 2
+
+
+def divisible_by_2(my_list=[]):
+    a = my_list[:]
+    for i in my_list:
+        if my_list[i] % 2 == 0:
+            a[i] = True
+        else:
+            a[i] = False
+
+    return a
