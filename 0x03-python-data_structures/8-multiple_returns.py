a551e3ea993d793dad2ef9ac651ee7f7490157cc
aydentownsley <aydentownsley@gmail.com>
2020-12-16 01:27:55 +0000

    adding 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+# multiple_returns - return tuple with length of string
+# and first character of string
+#
+# @sentence: string
+#
+# Return: length and first char
+# OR 0 and None
+# OR None if no string
+
+
+def multiple_returns(sentence):
+    if not sentence:
+        return None
+    if len(sentence) == 0:
+        a = 0
+        b = None
+    else:
+        a = len(sentence)
+        b = sentence[0]
+
+    return (a, b)
