b04434faf039101b3616ebde966708fcf9bdca9b
aydentownsley <aydentownsley@gmail.com>
2020-12-18 19:49:42 +0000

    adding not a string case

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
     res = 0
     nmrls = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     newarr = [nmrls[i] for i in roman_string]
