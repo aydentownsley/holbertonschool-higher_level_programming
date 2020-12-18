41d0f08044dfe62ea23689bdf9f262769b72e471
aydentownsley <aydentownsley@gmail.com>
2020-12-18 17:22:15 +0000

    adding problem 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    res = 0
+    nmrls = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    newarr = [nmrls[i] for i in roman_string]
+    for j in range(len(newarr)):
+        if j != len(newarr) - 1:
+            if newarr[j] < newarr[j + 1]:
+                res = res - newarr[j]
+            else:
+                res = res + newarr[j]
+        else:
+            res = res + newarr[j]
+    return res
