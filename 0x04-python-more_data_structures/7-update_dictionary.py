426b41c1f9cdd1d454cacd413e48cb6982dc6e64
aydentownsley <aydentownsley@gmail.com>
2020-12-18 01:36:28 +0000

    addin problem 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+# update_dictionary - repalces or adds values to dict
+#
+# @a_dictionary: dict
+# @key: key to add or replace
+# @value: value to add
+#
+# Return: updated dict
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
