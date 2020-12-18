555c7f5a22d4e3fbef0bf2f1dfdd1f833f30636f
aydentownsley <aydentownsley@gmail.com>
2020-12-18 01:06:41 +0000

    adding problem 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#
+# common_elements - returns common elements in two sets
+#
+# @set_1: set to search
+# @set_2: set to search
+#
+# Return: common elements
+
+
+def common_elements(set_1, set_2):
+    if (set_1 & set_2):
+        return set_1 & set_2
