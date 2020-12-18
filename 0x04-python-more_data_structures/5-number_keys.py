31746d79b874fb8edbd6893a712b7d0c5401d8fe
aydentownsley <aydentownsley@gmail.com>
2020-12-18 01:23:12 +0000

    adding number 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#
+# number_keys - returns num of keys in dict
+#
+# @a_dictionary: dictionary
+#
+# Return: num of keys
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary)
