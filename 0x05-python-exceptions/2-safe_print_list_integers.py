fd22bbacc76c79baff34fdecd90d68e8eb125773
aydentownsley <aydentownsley@gmail.com>
2021-01-05 01:38:17 +0000

    updating 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,16 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     idx = 0
+    pnum = 0
     try:
         for idx in range(x):
             if type(my_list[idx]) is int:
                 print(my_list[idx], end="")
-            idx = idx + 1
+                pnum += 1
+            idx += 1
         print()
-        return idx
+        return pnum
     except:
+        raise
         print("IndexError: list index out of range")
-        return idx
+        return pnum
