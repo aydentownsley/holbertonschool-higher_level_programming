ac9b752f3be3d615745ea989784b393d790380a5
aydentownsley <aydentownsley@gmail.com>
2021-01-05 01:40:49 +0000

    changed print format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for idx in range(x):
             if type(my_list[idx]) is int:
-                print(my_list[idx], end="")
+                print("{:d}".format(my_list[idx]), end="")
                 pnum += 1
             idx += 1
         print()
