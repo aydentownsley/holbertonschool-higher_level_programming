baaca12c60cee73c8019ddc6e19311fea27bb8ff
aydentownsley <aydentownsley@gmail.com>
2021-01-06 00:01:54 +0000

    rewrote, turns out it was just an indentation issue

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,12 @@ class Square:
     """ def of square """
 
     def __init__(self, size=0):
-        """ create square
-            size : size of square
+        """ create a square
+            size set to zero
+            raise Type and Value
         """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
