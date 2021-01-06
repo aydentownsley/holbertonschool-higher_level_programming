59d5cc2860b5327147ff9e8b3b55e29120b55f8f
aydentownsley <aydentownsley@gmail.com>
2021-01-06 17:18:19 +0000

    changed variable names for giggles

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+def magic_calculation(elephant, bongo):
+    sqwuack = 0
+    for rant in range(1, 3):
         try:
-            if i > a:
+            if rant > elephant:
                 raise Exception('Too Far')
 
-            result = result + (a ** b) / i
+            sqwuack = sqwuack + (elephant ** bongo) / rant
         except:
-            result = a + b
+            sqwuack = elephant + bongo
             break
-    return result
+    return sqwuack
