0b1caefdf59cf66d2bfbe4d79c1463839eb75eda
aydentownsley <aydentownsley@gmail.com>
2021-01-09 06:03:46 +0000

    pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,28 @@
+#!/usr/bin/python3
+
+
+"""
+Unit Test for Max Int
+"""
+
 import unittest
+max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
-    """ Unit Test for Max Int """
 
     def test_max(self):
-        """ tests valid input """
+        """ test valid input """
 
         self.assertEqual(max_integer([1, 2, 4, 8, 16]), 16)
         self.assertEqual(max_integer([2, -6, 0, -99, 32]), 32)
         self.assertEqual(max_integer([-13, -4, -5, 0, -12]), 0)
         self.assertEqual(max_integer([-300]), -300)
-        self.assertEqual(max_integer([], None)
+        self.assertEqual(max_integer([]), None)
 
     def test_type(self):
-        """ test type input """
+        """ test othe input
+        """
 
-        self.assertRaises(TypeError, max_integer(), ["toast", "brush", "can", "sing"])
+        self.assertRaises(TypeError, max_integer(), ["cat", "dog"])
         self.assertRaises(TypeError, max_integer(), [0.0, 4.3, 9.0])
