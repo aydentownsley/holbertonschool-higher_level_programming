52b29d836e7b8b4ba20daf2ba455465cebbb70d8
aydentownsley <aydentownsley@gmail.com>
2021-01-12 00:21:25 +0000

    changing typos from 1 and 2

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -45,9 +45,9 @@ class Rectangle:
             of type int
         """
         if type(value) is not int:
-            raise TypeError("height muse be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height myst be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
@@ -56,7 +56,7 @@ class Rectangle:
             retreives heigh and width and
             returns the product of the two
         """
-        if self.__width is 0 or self.__width is 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
         return self.__height * self.__width
 
@@ -67,7 +67,7 @@ class Rectangle:
             mult both by do and return
             the sum.
         """
-        if self.__width is 0 or self.__width is 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
