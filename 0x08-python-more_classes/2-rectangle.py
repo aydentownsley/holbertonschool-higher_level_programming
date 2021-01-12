0c189b54ee0fef04d0a3c1a7639da3c213be83a6
aydentownsley <aydentownsley@gmail.com>
2021-01-12 00:15:59 +0000

    adding changes from 1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -45,9 +45,9 @@ class Rectangle:
             of type int
         """
         if type(value) is not int:
-            raise TypeError("height muse be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height myst be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
