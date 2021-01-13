edd51d5092ef618e22f5364578eb327fa75ebe4b
aydentownsley <aydentownsley@gmail.com>
2021-01-13 01:21:10 +0000

    pep8

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+""" locked class module """
+
+
 class LockedClass():
+    """ Locked class
+
+        first_name: only user
+        accesible element
+    """
     __slots__ = ['first_name']
