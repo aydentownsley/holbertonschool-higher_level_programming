56c9449382acc0c9f6ccba1161f6087560ac1573
aydentownsley <aydentownsley@gmail.com>
2021-01-20 04:58:59 +0000

    replaced int valid with cleaer application

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -28,8 +28,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ init
         """
-        BaseGeometry.integer_validator(self, 'width', width)
-        BaseGeometry.integer_validator(self, 'height', height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
 
