22f1b68f9a887021a4721b002227431e564a3135
aydentownsley <aydentownsley@gmail.com>
2021-01-20 03:48:11 +0000

    adding 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" BaseGeo Module """
+
+
+class BaseGeometry:
+    """ base geo class
+    """
+
+    def area(self):
+        """
+            self
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ int validate
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be an integer".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle
+    """
+
+    def __init__(self, width, height):
+        """ init
+        """
+        BaseGeometry.integer_validator(self, 'width', width)
+        BaseGeometry.integer_validator(self, 'height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ returns width * height
+        """
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """ String form
+        """
+        return ("[Retangle] {}/{}".format(self.__width, self.__height))
