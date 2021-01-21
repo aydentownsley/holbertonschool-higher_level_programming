f5e4dbe2cb5f6f076a08fa0ef0559c434f854007
aydentownsley <aydentownsley@gmail.com>
2021-01-21 00:46:46 +0000

    changed imort location

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,10 +3,11 @@
 """
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
         my_obj: object to be saved
         filename: location to save to
     """
-    with open(filename, 'r+') as f:
+    with open(filename, 'w') as f:
         return (f.write(json.dumps(my_obj)))
