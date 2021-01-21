ef0e966141c0115757d064c840104814f0576722
aydentownsley <aydentownsley@gmail.com>
2021-01-21 00:16:46 +0000

    adding mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
         read and prints a
         UTF8 file
     """
-    f = open(filename, encoding='utf-8')
+    f = open(filename, mode='r', encoding='utf-8')
     for line in f:
         print(line, end="")
