0067c7199cf256a6ca6e087e42d200ec199221dd
aydentownsley <aydentownsley@gmail.com>
2021-01-21 00:26:28 +0000

    changed to write only

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ def write_file(filename="", text=""):
         filename: name of file
         text: text to write in
     """
-    with open(filename, mode='r+', encoding='utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         return (f.write(text))
