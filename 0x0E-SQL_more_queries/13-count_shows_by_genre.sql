28f9f5dcb01a9d80cdc3b152906b2045459987cb
aydentownsley <aydentownsley@gmail.com>
2021-03-02 23:36:14 +0000

    adding 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- SQL SCRIPT
+-- DISPLAY TABLE WITH genre and # of
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
