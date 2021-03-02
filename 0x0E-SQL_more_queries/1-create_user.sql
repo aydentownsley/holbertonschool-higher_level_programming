ce285c9add9d1b93fa709d35b7bacc86bcde5508
aydentownsley <aydentownsley@gmail.com>
2021-03-02 19:58:42 +0000

    fixing line break

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- SQL SCRIPT
 -- CREATING USER
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
