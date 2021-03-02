a1d9ee7ad87ac9a9286bca5db6e0dc3582fc21ce
aydentownsley <aydentownsley@gmail.com>
2021-03-02 21:46:05 +0000

    forgot that the uppercase was requirement

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- SQL SCRIPT
--- LIST AL CA CITIES IN DATABASE (using lowecase sql)
-select id, name from hbtn_0d_usa.cities where state_id=(select id from hbtn_0d_usa.states where name="California") order by id asc;
+-- LIST AL CA CITIES IN DATABASE
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id=(SELECT id FROM hbtn_0d_usa.states WHERE name="California") ORDER BY id ASC;
