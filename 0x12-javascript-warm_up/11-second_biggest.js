c59f0ddefd824617f4a6fb78dc0453769dbaa645
aydentownsley <aydentownsley@gmail.com>
2021-03-22 22:07:35 +0000

    adding 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  let max = Number(process.argv[2]);
+  let secMax;
+  for (let i = 0; i < process.argv.length - 1; i++) {
+    if (max < Number(process.argv[i + 1])) {
+      secMax = max;
+      max = Number(process.argv[i + 1]);
+    } else if (Number(process.argv[i + 1] > secMax) && Number(process.argv[i + 1]) < max) {
+      secMax = Number(process.argv[i + 1]);
+    }
+  }
+  console.log(secMax);
+}
