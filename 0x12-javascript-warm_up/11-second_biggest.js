36364468a899a1082e1ae82e6823eb426731d308
aydentownsley <aydentownsley@gmail.com>
2021-03-22 22:23:18 +0000

    fix for when largest is first

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,12 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let max = Number(process.argv[2]);
-  let secMax;
-  for (let i = 0; i < process.argv.length - 1; i++) {
-    if (max <= Number(process.argv[i + 1])) {
+  let secMax = Number(process.argv[3]);
+  for (let i = 0; i < process.argv.length; i++) {
+    if (max < Number(process.argv[i + 1])) {
       secMax = max;
       max = Number(process.argv[i + 1]);
-    } else if (Number(process.argv[i + 1] >= secMax) && Number(process.argv[i + 1]) <= max) {
+    } else if (Number(process.argv[i + 1] > secMax) && Number(process.argv[i + 1]) < max) {
       secMax = Number(process.argv[i + 1]);
     }
   }
