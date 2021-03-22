79cfe9ad8f1674ef17a9c9879b3f1b3113957db1
aydentownsley <aydentownsley@gmail.com>
2021-03-22 21:09:34 +0000

    typo, awesome to amazing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'JavaScript is awesome'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i <= 2; i++) {
   console.log(strings[i]);
 }
