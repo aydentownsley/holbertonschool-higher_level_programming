a4ac6ff40e5e84af6a83cadb9d5dee35c704934e
aydentownsley <aydentownsley@gmail.com>
2021-03-24 00:12:13 +0000

    semi fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if ( !w || !h || w <= 0 || h <= 0) {
+    if (!w || !h || w <= 0 || h <= 0) {
       // pass
     } else {
       this.width = w;
