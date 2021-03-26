87034cbd1cc75b0f6858b07b337dfbbdafede8db
aydentownsley <aydentownsley@gmail.com>
2021-03-26 20:13:22 +0000

    adding 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists all states starting with 'N'"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    uname = sys.argv[1]
+    pword = sys.argv[2]
+    dbname = sys.argv[3]
+    stname = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=uname,
+                         passwd=pword, db=dbname, charset="utf8")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE"
+                " name = %s ORDER BY id ASC", [stname])
+    print(cur.fetchone())
+    cur.close()
+    db.close()
