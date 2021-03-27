731c6da88dea9dcd529b5faf799afebf20540aba
aydentownsley <aydentownsley@gmail.com>
2021-03-27 20:21:07 +0000

    adding fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,5 @@ class State(Base):
     """ Links to MySQL states
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, unique=True,
-                autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
