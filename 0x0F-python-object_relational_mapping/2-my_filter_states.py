998ae28d48664af477cfb4105b12cd5d8bf48e63
aydentownsley <aydentownsley@gmail.com>
2021-03-27 20:00:35 +0000

    tyring to fix 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """Lists all states starting with 'N'"""
+import MySQLdb
+import sys
+
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
 
     uname = sys.argv[1]
     pword = sys.argv[2]
     dbname = sys.argv[3]
     stname = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=uname,
-                         passwd=pword, db=dbname, charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=uname, passwd=pword, db=dbname)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE "
-                "BINARY name='{:s}' ORDER BY id ASC"
+    cur.execute("SELECT id, name FROM states WHERE "
+                "BINARY name='{}' ORDER BY id ASC"
                 .format(stname))
     for row in cur.fetchall():
         print(row)
