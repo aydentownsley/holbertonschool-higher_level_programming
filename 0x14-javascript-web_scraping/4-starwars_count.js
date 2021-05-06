dc41b5cefb2ac6fcf6dc14e6718d0048fd62e82c
aydentownsley <aydentownsley@gmail.com>
2021-05-06 07:24:06 +0000

    adding 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv.slice(2);
+const character = 'https://swapi-api.hbtn.io/api/people/18/';
+let i; let k = 0;
+
+request.get(url.toString(), function (err, body) {
+  if (err) {
+    return console.log(err);
+  } else {
+    const dict = JSON.parse(body.body);
+    for (i = 0; i < 7; i++) {
+      if (dict.results[i].characters.includes(character)) {
+        k++;
+      }
+    }
+    return console.log(k);
+  }
+});
