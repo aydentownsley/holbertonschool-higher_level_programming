81187332c0c6c35b75e635e6f1de7d669bc61ccd
aydentownsley <aydentownsley@gmail.com>
2021-05-06 16:20:34 +0000

    using id and better loop method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv.slice(2);
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
-let i; let k = 0;
+const url = process.argv.slice(2).toString();
+let k = 0;
 
-request.get(url.toString(), function (err, body) {
+request(url, function (err, body) {
   if (err) {
     return console.log(err);
   } else {
-    const dict = JSON.parse(body.body);
-    for (i = 0; i < 7; i++) {
-      if (dict.results[i].characters.includes(character)) {
-        k++;
+    const resArray = JSON.parse(body.body).results;
+    for (const i in resArray) {
+      for (const x in resArray[i].characters) {
+        if (resArray[i].characters[x].includes('18')) {
+          k++;
+        }
       }
     }
     console.log(k);
