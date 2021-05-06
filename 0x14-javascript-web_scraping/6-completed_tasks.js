b1bea091123e7d97b52b82afb05b795a75d0202a
aydentownsley <aydentownsley@gmail.com>
2021-05-06 18:28:48 +0000

    adding 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2].toString();
+let k = 1;
+const newDict = {};
+let key = 1;
+let data;
+
+request(url, function (err, res) {
+  if (err) {
+    return console.log(err);
+  } else {
+    data = JSON.parse(res.body);
+    for (const i in data) {
+      if (data[i].completed === true) {
+        if (key !== data[i].userId) { k = 1; }
+        key = data[i].userId;
+        newDict[key] = k++;
+      }
+    }
+
+    console.log(newDict);
+  }
+});
